6697eb20f96f9b1efb09c5a942abe841bcee417e
Practice2001 <40858007+Jarvis2001@users.noreply.github.com>
2025-06-19 17:31:20 +0530

    Update test_clean_sensor.py

--- tests/sensor_tests/test_clean_sensor.py
+++ tests/sensor_tests/test_clean_sensor.py
@@ -7,18 +7,20 @@ def test_temperature_conversion():
     result = clean_sensor_data(input_data)
     assert result["temperature_k"] == pytest.approx(298.15, rel=1e-5)
 
+
 def test_humidity_conversion():
     input_data = {"humidity": "80"}
     result = clean_sensor_data(input_data)
     assert result["humidity"] == 80.0
+
 
 def test_wind_speed_conversion():
     input_data = {"wind_speed_mps": 10}
     result = clean_sensor_data(input_data)
     assert result["wind_speed_kph"] == pytest.approx(36.0, rel=1e-5)
 
+
 def test_boolean_conversion():
     input_data = {"anamoly_detected": "True"}
     result = clean_sensor_data(input_data)
     assert result["anomaly_detected"] is True
-
